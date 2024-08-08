1fbe66014959e8584283b4f349715d4f80642c57
CompSciHigh24 <csh-github@compscihigh.org>
2024-08-08 18:11:44 -0400

    Initial Commit

---
+++ index.js
@@ -0,0 +1,60 @@
+// Exercise 1: Age Checker
+// Ask the user to enter their age. If they are 18 or older, display "You are an adult." Otherwise, display "You are not an adult."
+
+let user = prompt("what's your age?")
+let age = 18;
+if (user >= 18){
+  console.log("You are an adult");
+}else {
+  console.log("You are not an adult")
+}
+
+
+
+// Exercise 2: Simple Comparison
+// Write a program that prompts the user to enter two numbers and displays whether the first number is greater, less than, or equal to the second number.
+// Note: make sure you have the correct types!
+
+let numberOne = prompt("enter a number")
+let numberTwo = prompt("enter a number")
+
+if (numberOne > numberTwo){
+  console.log("Your first number is greater than the second number")
+} else if(numberOne < numberTwo){
+  console.log("your first number is less than yu second number")
+}else if (numberOne == numberTwo){
+  console.log("your first number is equal to your second number")
+}
+
+
+// Exercise 3: Even or Odd
+// Create a program that asks the user for an integer and then prints whether the number is even or odd. (Google how to check this!)
+
+
+
+
+// Exercise 4: Day of the Week
+// Prompt the user to enter a day of the week (e.g., "Monday"). Display a message that says whether it's a weekday or the weekend.
+
+
+
+
+
+// Exercise 5: Age Group Classifier
+// Prompt the user to enter their age and categorize them into different age groups:
+// - 0-12: Child
+// - 13-19: Teenager
+// - 20-64: Adult
+// - 65+: Senior Citizen
+
+
+
+
+
+// Exercise 6: Rock, Paper, Scissors Game
+// Create a simple rock, paper, scissors game. Prompt the user to enter their choice (rock, paper, or scissors) and then randomly generate the computer's choice. Determine the winner and display the result.
+
+
+
+
+
